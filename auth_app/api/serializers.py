0cd0cb9c39f725c9ebbd47730c764c813cc1658d
KKrntzk <sophie.kornetzki@gmail.com>
2026-07-06 11:14:53 +0200

    feat(auth_app): add RegistrationSerializer with password validation

---
+++ auth_app/api/serializers.py
@@ -0,0 +1,32 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class RegistrationSerializer(serializers.ModelSerializer):
+    repeated_password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ["username", "email", "password", "repeated_password", "type"]
+        extra_kwargs = {
+            "password": {"write_only": True},
+        }
+
+    def validate(self, data):
+        if data["password"] != data["repeated_password"]:
+            raise serializers.ValidationError(
+                {"repeated_password": "Passwords do not match."}
+            )
+        return data
+
+    def create(self, validated_data):
+        validated_data.pop("repeated_password")
+        user = User.objects.create_user(
+            username=validated_data["username"],
+            email=validated_data["email"],
+            password=validated_data["password"],
+            type=validated_data["type"],
+        )
+        return user
