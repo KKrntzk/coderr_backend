57ba7340fb3a564621c93e93d52d89362ecc7f7d
KKrntzk <sophie.kornetzki@gmail.com>
2026-07-06 11:25:26 +0200

    feat(auth_app): register CustomUser in admin with type field

--- auth_app/admin.py
+++ auth_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from auth_app.models import CustomUser
 
-# Register your models here.
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (("Additional Info", {"fields": ("type",)}),)
