7a429fc3339806eee8e289af4889f5868c21af1b
KKrntzk <sophie.kornetzki@gmail.com>
2026-07-06 11:38:31 +0200

    feat(profiles_app): add ProfileDetailView with GET

--- profiles_app/api/views.py
+++ profiles_app/api/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+from profiles_app.api.serializers import ProfileSerializer
+
+User = get_user_model()
+
+
+class ProfileDetailView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self, pk):
+        try:
+            return User.objects.get(pk=pk)
+        except User.DoesNotExist:
+            return None
+
+    def get(self, request, pk):
+        user = self.get_object(pk)
+        if user is None:
+            return Response(
+                {"detail": "Profile not found."}, status=status.HTTP_404_NOT_FOUND
+            )
+        serializer = ProfileSerializer(user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
