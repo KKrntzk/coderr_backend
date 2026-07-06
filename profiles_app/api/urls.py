2e8eaddbc09a01c6b41afb0eed3a76af6d357b6e
KKrntzk <sophie.kornetzki@gmail.com>
2026-07-06 11:39:00 +0200

    feat(profiles_app): add profile detail endpoint and wire up urls

---
+++ profiles_app/api/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from profiles_app.api.views import ProfileDetailView
+
+urlpatterns = [
+    path("profile/<int:pk>/", ProfileDetailView.as_view(), name="profile-detail"),
+]
