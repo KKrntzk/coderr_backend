185c80a195debc6a495c58d553ba5235c1e6f3ca
KKrntzk <sophie.kornetzki@gmail.com>
2026-07-07 09:13:55 +0200

    feat(profiles_app): add business profile list endpoint

--- profiles_app/api/urls.py
+++ profiles_app/api/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from profiles_app.api.views import ProfileDetailView
+from profiles_app.api.views import ProfileDetailView, BusinessProfileListView
 
 urlpatterns = [
     path("profile/<int:pk>/", ProfileDetailView.as_view(), name="profile-detail"),
+    path(
+        "profiles/business/",
+        BusinessProfileListView.as_view(),
+        name="business-profile-list",
+    ),
 ]
