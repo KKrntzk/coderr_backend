4f14a4e23d4860b9381cf49132139bdc02f569f1
KKrntzk <sophie.kornetzki@gmail.com>
2026-07-09 12:46:24 +0200

    feat(offers_app): add offer list endpoint and wire up urls

--- core/urls.py
+++ core/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("auth_app.api.urls")),
     path("api/", include("profiles_app.api.urls")),
+    path("api/", include("offers_app.api.urls")),
 ]
