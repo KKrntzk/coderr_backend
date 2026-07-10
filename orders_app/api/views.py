24c1b8c7e2de1aa176d5b87b49d4c9e8a8621a5e
KKrntzk <sophie.kornetzki@gmail.com>
2026-07-10 09:10:12 +0200

    feat(orders_app): add OrderListView

--- orders_app/api/views.py
+++ orders_app/api/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from rest_framework.permissions import IsAuthenticated
+from django.db.models import Q
 
-# Create your views here.
+from orders_app.models import Order
+from orders_app.api.serializers import OrderSerializer
+
+
+class OrderListView(ListAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = OrderSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        return Order.objects.filter(Q(customer_user=user) | Q(business_user=user))
