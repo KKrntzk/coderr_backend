9be301fc1f8f47f1ded0d944ae7990cd010b2a66
KKrntzk <sophie.kornetzki@gmail.com>
2026-07-15 11:30:37 +0200

    feat(orders_app): add order count endpoint

--- orders_app/api/urls.py
+++ orders_app/api/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
-from orders_app.api.views import OrderListCreateView, OrderUpdateDestroyView
+from orders_app.api.views import (
+    OrderListCreateView,
+    OrderUpdateDestroyView,
+    OrderCountView,
+)
 
 urlpatterns = [
     path("orders/", OrderListCreateView.as_view(), name="order-list-create"),
     path("orders/<int:pk>/", OrderUpdateDestroyView.as_view(), name="order-update"),
+    path(
+        "order-count/<int:business_user_id>/",
+        OrderCountView.as_view(),
+        name="order-count",
+    ),
 ]
