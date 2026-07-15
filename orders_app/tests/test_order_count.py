6d66b8d40787d5a689c5cd97970139b3bbd6ecd8
KKrntzk <sophie.kornetzki@gmail.com>
2026-07-15 12:32:54 +0200

    test(orders_app): add CompletedOrderCountView tests

--- orders_app/tests/test_order_count.py
+++ orders_app/tests/test_order_count.py
@@ -84,3 +84,81 @@ class OrderCountViewTest(APITestCase):
         self.client.credentials()
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class CompletedOrderCountViewTest(APITestCase):
+
+    def setUp(self):
+        self.customer = User.objects.create_user(
+            username="testcustomer",
+            email="customer@mail.de",
+            password="testpassword123",
+            type="customer",
+        )
+        self.business = User.objects.create_user(
+            username="testbusiness",
+            email="business@mail.de",
+            password="testpassword123",
+            type="business",
+        )
+        self.token = Token.objects.create(user=self.customer)
+        self.client.credentials(HTTP_AUTHORIZATION="Token " + self.token.key)
+
+        Order.objects.create(
+            customer_user=self.customer,
+            business_user=self.business,
+            title="Order 1",
+            revisions=3,
+            delivery_time_in_days=5,
+            price=150,
+            features=[],
+            offer_type="basic",
+            status="completed",
+        )
+        Order.objects.create(
+            customer_user=self.customer,
+            business_user=self.business,
+            title="Order 2",
+            revisions=3,
+            delivery_time_in_days=5,
+            price=150,
+            features=[],
+            offer_type="basic",
+            status="completed",
+        )
+        Order.objects.create(
+            customer_user=self.customer,
+            business_user=self.business,
+            title="Order 3",
+            revisions=3,
+            delivery_time_in_days=5,
+            price=150,
+            features=[],
+            offer_type="basic",
+            status="in_progress",
+        )
+        self.url = reverse(
+            "completed-order-count", kwargs={"business_user_id": self.business.id}
+        )
+
+    def test_completed_order_count_success(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["completed_order_count"], 2)
+
+    def test_completed_order_count_business_user_not_found(self):
+        url = reverse("completed-order-count", kwargs={"business_user_id": 9999})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_completed_order_count_user_is_customer_not_business(self):
+        url = reverse(
+            "completed-order-count", kwargs={"business_user_id": self.customer.id}
+        )
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_completed_order_count_unauthenticated(self):
+        self.client.credentials()
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
