011f2aab3e3b2624dbea43439956bb8e80354ac7
KKrntzk <sophie.kornetzki@gmail.com>
2026-07-15 09:47:16 +0200

    feat(orders_app): add order update endpoint

--- orders_app/api/urls.py
+++ orders_app/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from orders_app.api.views import OrderListCreateView
+from orders_app.api.views import OrderListCreateView, OrderUpdateView
 
 urlpatterns = [
     path("orders/", OrderListCreateView.as_view(), name="order-list-create"),
+    path("orders/<int:pk>/", OrderUpdateView.as_view(), name="order-update"),
 ]
