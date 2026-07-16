5cd18dc3d6ee6d646fb11eda80cd6391e5bcdb0e
KKrntzk <sophie.kornetzki@gmail.com>
2026-07-16 16:01:12 +0200

    feat(reviews_app): add review update/destroy endpoint

--- reviews_app/api/urls.py
+++ reviews_app/api/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from reviews_app.api.views import ReviewListCreateView
+from reviews_app.api.views import ReviewListCreateView, ReviewUpdateDestroyView
 
 urlpatterns = [
     path("reviews/", ReviewListCreateView.as_view(), name="review-list-create"),
+    path(
+        "reviews/<int:pk>/",
+        ReviewUpdateDestroyView.as_view(),
+        name="review-update-destroy",
+    ),
 ]
