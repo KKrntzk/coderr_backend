10ffa84fb13177b68e57cf1237d028fc3782424c
KKrntzk <sophie.kornetzki@gmail.com>
2026-07-16 13:39:17 +0200

    feat(reviews_app): register Review in admin

--- reviews_app/admin.py
+++ reviews_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from reviews_app.models import Review
 
-# Register your models here.
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ["business_user", "reviewer", "rating", "created_at"]
