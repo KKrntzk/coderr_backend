ea8d5d0108561fd96ae2882964bb4613b6fc33eb
KKrntzk <sophie.kornetzki@gmail.com>
2026-07-16 13:43:18 +0200

    feat(reviews_app): add ReviewSerializer

---
+++ reviews_app/api/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from reviews_app.models import Review
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = [
+            "id",
+            "business_user",
+            "reviewer",
+            "rating",
+            "description",
+            "created_at",
+            "updated_at",
+        ]
+        read_only_fields = fields
